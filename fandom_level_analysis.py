47e406b8aabb00800826529806f7043c47c87918
pharsaliam <jenniferz7633@gmail.com>
2021-12-26 18:49:31 -0800

    Add relationship type selector, text, reformat

--- fandom_level_analysis.py
+++ fandom_level_analysis.py
@@ -12,17 +12,24 @@ FANDOM_ORDER_LU = {
 }
 
 
+st.title('AO3 Data Visualizations')
+st.markdown('''
+This page displays some summary charts of the works on AO3 for a selected fandom, as of Feb. 26th, 2021. 
+The data used was provided by AO3 in their [March 2021 data dump](https://archiveofourown.org/admin_posts/18804). 
+''')
 non_fandom_tags_agg, works_with_fandom, fandom_works_count = retrieve_preprocessed_data()
-fandom_order = st.radio('View fandom list ordered by', [s for s in FANDOM_ORDER_LU.keys()])
+col1, col2 = st.columns([1, 2])
+fandom_order = col1.radio('View fandom list ordered by', [s for s in FANDOM_ORDER_LU.keys()])
 fandom_select_list = fandom_works_count.sort_values(**FANDOM_ORDER_LU[fandom_order])
-FANDOM = st.selectbox('Choose fandom', fandom_select_list)
+FANDOM = col2.selectbox('Choose fandom', fandom_select_list)
 logger.info(f'Initializing fandom class for {FANDOM}')
 fandom = Fandom(FANDOM, works_with_fandom, non_fandom_tags_agg)
 logger.info(f'{FANDOM} initialized')
-st.text(f'We found {format_number(len(fandom.works))} fics to analyze.')
+st.markdown(f'We found __{format_number(len(fandom.works))}__ fics to analyze.')
 st.subheader('Relationship Chord Chart')
+relationship_type = st.radio('Choose relationship type', ['romantic', 'platonic'])
 fig_c, ax_c = plt.subplots()
-fandom.generate_relationship_chord_chart(ax=ax_c)
+fandom.generate_relationship_chord_chart(relationship_type=relationship_type, ax=ax_c)
 st.pyplot(fig_c)
 st.subheader('Word Count Distribution')
 fig, ax = plt.subplots()
